cbbf7245c20da6808021b9cd9fb136b3a0e3b25c
Daisuke0655 <85994195+Daisuke0655@users.noreply.github.com>
2024-05-07 15:06:40 +0900

    Revert "react-router-domの追加"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from "react";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-
-const App = () =>{
-    return(
-        <div>
-            <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<null />}></Route>
-            </Routes>
-            </BrowserRouter>
-        </div>
-    )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
