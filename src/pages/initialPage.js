77805140b6b6c25f0e1d253914c5a390ec0ba89b
NishioHaruki <3j20nshio@gmail.com>
2024-05-07 15:35:19 +0900

    add initialPage.js and .css プロトタイプ

---
+++ src/pages/initialPage.js
@@ -0,0 +1,15 @@
+import './initialPage.css';
+
+function initialPage() {
+  return (
+    <div className="initialPage">
+      <h1>Welcome to CHaser</h1>
+      <button className="button-style">対戦を始める</button>
+      <button className="button-style">なにか</button>
+      <button className="button-style">なにか2</button>
+      <button className="button-style">大会に参加</button>
+    </div>
+  );
+}
+
+export default initialPage
