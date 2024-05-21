14ae87b055d5ab863e9aed7faedadb1cb1378b5c
Daisuke0655 <s20009@tokyo.kosen-ac.jp>
2024-05-21 15:30:29 +0900

    add page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import InitialPage from "./pages/InitiaPage/InitialPage";
+import InitialPage from "./pages/Initial/Initial";
 import Login from "./pages/Login/Login";
+import Log from "./pages/Log/Log"
 
 
 const App = () =>{
@@ -11,6 +12,7 @@ const App = () =>{
             <Routes>
                 <Route path="/" element={<Login />}></Route>
                 <Route path="/initial" element={<InitialPage />}></Route>
+                <Route path="/log" element = {<Log />}></Route>
             </Routes>
             </BrowserRouter>
         </div>
---
+++ src/components/receiveLog.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const receiveLog = async () => {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: ''
+  };
+
+  try {
+    fetch('http://localhost:8000/Log', requestOptions)
+    .then(response => response.json())
+    .then(jsonObj => {
+        console.log(jsonObj.message);
+        return jsonObj.message
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;  // 必要に応じてエラーを再スロー
+  }
+};
+
+export default receiveLog;
