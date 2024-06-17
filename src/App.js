c4da27f1e8145dd88232f9d206b01e148546093a
Daisuke0655 <s20009@tokyo.kosen-ac.jp>
2024-06-17 13:02:15 +0900

    add /editmap route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Log from "./pages/Log/Log"
 import Match from './pages/Match/Match'
 import SelectEnem from './pages/SelectEnem/SelectEnem'
 import EditScript from "./pages/EditScript/EditScript"
+import EditMap from "./pages/EditMap/EditMap"
 
 
 const App = () =>{
@@ -18,6 +19,7 @@ const App = () =>{
                 <Route path="/initial" element={<InitialPage />}></Route>
                 <Route path="/log" element = {<Log />}></Route>
                 <Route path="/edit" element = {<EditScript />}></Route>
+                <Route path="/editMap" element = {<EditMap />}></Route>
                 <Route path="/select" element = {<SelectEnem/>}></Route>
                 <Route path="/match" element = {<Match/>}></Route>
             </Routes>
