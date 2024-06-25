573fb38e1e4103a5ee0d31e2bae7af33b7a4a406
U-KITAZAWA <tai.0218.san@gmail.com>
2024-06-26 01:00:10 +0900

    update: variable color code in css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -50,36 +50,42 @@ const Login = () => {
   };
 
   return (
-    <div className="container">
-      <h1 className="heading">ログイン画面</h1>
-      <form className="form" onSubmit={handleSubmit}>
-        <div className="form-field">
-          <label className="label">名前:</label>
-          {errors.name && <span className="error">{errors.name}</span>}
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            className="input"
-          ></input>
+    <>
+      <div className="login">
+        <div className="container">
+          <h1 className="heading">ログイン</h1>
+          <form className="form" onSubmit={handleSubmit}>
+            <div className="form-field">
+              <label className="label">名前:</label>
+              {errors.name && <span className="error">{errors.name}</span>}
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="input"
+              ></input>
+            </div>
+            <div className="form-field">
+              <label className="label">パスワード:</label>
+              {errors.password && (
+                <span className="error">{errors.password}</span>
+              )}
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="input"
+              ></input>
+            </div>
+            <button type="submit" className="button">
+              ログイン
+            </button>
+          </form>
         </div>
-        <div className="form-field">
-          <label className="label">パスワード:</label>
-          {errors.password && <span className="error">{errors.password}</span>}
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="input"
-          ></input>
-        </div>
-        <button type="submit" className="button">
-          ログイン
-        </button>
-      </form>
-    </div>
+      </div>
+    </>
   );
 };
 
